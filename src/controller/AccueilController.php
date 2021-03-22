22ba5d76813866d1d1410abec1a26a74020fb4b5
29010-33-17 <29010-33-17@exchange.ad.afpanet>
2021-03-22 11:51:02 +0100

    AjoutAccueil

---
+++ src/controller/AccueilController.php
@@ -0,0 +1,11 @@
+<?php
+
+class AccueilController{
+    public function index(){
+        ob_start();
+        require_once BACK_ROOT  . '/views/accueil.php';
+        $view = ob_get_clean();
+        require_once(BACK_ROOT . '/views/template.php');
+    }
+}
+?>
