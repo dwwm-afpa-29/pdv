cb1b02aec7a7f0ab6d65fa0ab47c6bc1488c6996
Yannick Biheul <yannickbiheul@outlook.fr>
2021-04-22 09:56:47 +0200

    Réglage panier

--- src/views/template.php
+++ src/views/template.php
@@ -26,6 +26,8 @@
 
 <body>
 
+
+
     <!-- HEADER -->
     <header>
 
@@ -77,10 +79,12 @@
     // } 
     ?>
 
+
+
     <!-- NAV -->
     <nav>
 
-    <div hidden id="snipcart" style="z-index:100;" data-api-key="ZWMyYTgyYzItNmZkMC00ZDAzLTg0NmQtODgxYzJhN2Q4NDcyNjM3NTQxODI5NjY2Nzc3NzI4"data-currency="eur"></div>
+    
 
         <!-- VINS -->
 
@@ -138,7 +142,7 @@
     <!-- JAVASCRIPT -->
     <script src='assets/js/main.js'></script>
     <script async src="https://cdn.snipcart.com/themes/v3.1.0/default/snipcart.js"></script>
-    
+    <div hidden id="snipcart" style="z-index:100;" data-api-key="ZWMyYTgyYzItNmZkMC00ZDAzLTg0NmQtODgxYzJhN2Q4NDcyNjM3NTQxODI5NjY2Nzc3NzI4"data-currency="eur"></div>
     <script>
         document.addEventListener('snipcart.ready', () => {
             Snipcart.events.on('cart.confirmed', (cartConfirmResponse) =>{
